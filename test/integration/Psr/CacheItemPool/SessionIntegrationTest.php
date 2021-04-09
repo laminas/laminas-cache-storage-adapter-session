8aa699e981b4487d211d4ccf42545fde87b43de6
Gary Lockett <gary@creativecow.uk>
2021-04-09 16:18:10 +0100

    Add GHA CI workflow
    Signed-off-by: Gary Lockett <gary@creativecow.uk>

--- test/integration/Psr/CacheItemPool/SessionIntegrationTest.php
+++ test/integration/Psr/CacheItemPool/SessionIntegrationTest.php
@@ -8,6 +8,7 @@
 
 namespace LaminasTest\Cache\Psr\CacheItemPool;
 
+use Laminas\Cache\Psr\CacheItemPool\CacheException;
 use Laminas\Cache\Psr\CacheItemPool\CacheItemPoolDecorator;
 use Laminas\Cache\StorageFactory;
 use PHPUnit\Framework\TestCase;
@@ -16,11 +17,10 @@ class SessionIntegrationTest extends TestCase
 {
     /**
      * The session adapter doesn't support TTL
-     *
-     * @expectedException \Laminas\Cache\Psr\CacheItemPool\CacheException
      */
     public function testAdapterNotSupported()
     {
+        $this->expectException(CacheException::class);
         $storage = StorageFactory::adapterfactory('session');
         new CacheItemPoolDecorator($storage);
     }
